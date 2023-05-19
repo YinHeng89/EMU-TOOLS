fa47ccf195dcf8584716b97231468ac73a6ac811
YinHeng89 <k332159915@gmail.com>
2023-05-19 19:14:57 +0800

    更新css

--- index.php
+++ index.php
@@ -92,38 +92,36 @@ $conn->close();
 
 <body>
     <form>
-        <div>
-            <div class="menu">
-                <div class="menu-title">
-                    <img src="./images/logo.png" height="30px" width="30px">
-                    <span>EMU-Tools</span>
+        <div class="menu">
+            <div class="menu-title">
+                <img src="./images/logo.png" height="30px" width="30px">
+                <span>EMU-Tools</span>
+            </div>
+            <ul class="menu-list">
+                <li><a href="./index.php" target="_parent">Yuzu模拟器</a></li>
+                <li><a class="second" href="#" target="_parent">ryujinx模拟器</a></li>
+                <li><a class="second" href="#" target="_parent">固件下载</a></li>
+                <li><a class="second" href="#" target="_parent">其他工具</a></li>
+                <li><a class="second" href="#" target="_parent">工具简介</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <h1>Yuzu-EA 模拟器版本检测</h1>
+            <p>请点击下方按钮“检测更新”按钮检测最新版本。</p>
+            <form>
+                <!-- <label for="version">输入当前版本号，留空则自动检测最新版本：</label> -->
+                <div>
+                    <!-- <input type="text" id="version" name="version"> -->
+                    <label id="version">当前最新版本号为： <span id="latestVersion">-----</span></label>
+                    <input type="button" value="检测更新" onclick="checkVersion()">
                 </div>
-                <ul class="menu-list">
-                    <li><a href="./index.php" target="_parent">Yuzu模拟器</a></li>
-                    <li><a class="second" href="#" target="_parent">ryujinx模拟器</a></li>
-                    <li><a class="second" href="#" target="_parent">固件下载</a></li>
-                    <li><a class="second" href="#" target="_parent">其他工具</a></li>
-                    <li><a class="second" href="#" target="_parent">工具简介</a></li>
-                </ul>
-            </div>
-            <div class="main">
-                <h1>Yuzu-EA 模拟器版本检测</h1>
-                <p>请点击下方按钮“检测更新”按钮检测最新版本。</p>
-                <form>
-                    <!-- <label for="version">输入当前版本号，留空则自动检测最新版本：</label> -->
-                    <div>
-                        <!-- <input type="text" id="version" name="version"> -->
-                        <label id="version">当前最新版本号为： <span id="latestVersion">-----</span></label>
-                        <input type="button" value="检测更新" onclick="checkVersion()">
-                    </div>
-                    <div>
-                        <input type="button" value="下载最新版(Windows)" onclick="downloadLatest('Windows')" class="downloadBtn" disabled="disabled">
-                        <input type="button" value="下载最新版(Linux)" onclick="downloadLatest('Linux')" class="downloadBtn" disabled="disabled">
-                        <input type="button" value="检查并下载最新固件" onclick="downloadfirmware()">
-                    </div>
-                </form>
-                <p style="color: rgb(190, 190, 190);">本工具只用于检测yuzu模拟器最新版本,所有软件包来源于Github</p>
-            </div>
+                <div>
+                    <input type="button" value="下载最新版(Windows)" onclick="downloadLatest('Windows')" class="downloadBtn" disabled="disabled">
+                    <input type="button" value="下载最新版(Linux)" onclick="downloadLatest('Linux')" class="downloadBtn" disabled="disabled">
+                    <input type="button" value="检查并下载最新固件" onclick="downloadfirmware()">
+                </div>
+            </form>
+            <p style="color: rgb(190, 190, 190);">本工具只用于检测yuzu模拟器最新版本,所有软件包来源于Github</p>
         </div>
     </form>
     <p class="second">当前工具版本 v1.0.3 <a href="./update.php" target="_blank">查看更新记录</a>
