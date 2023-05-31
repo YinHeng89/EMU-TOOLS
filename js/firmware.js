4bbf5899ba2f28902c73272545b3da4811b8ef89
YinHeng89 <k332159915@gmail.com>
2023-05-31 14:05:59 +0800

    更新固件样式

--- js/firmware.js
+++ js/firmware.js
@@ -1,30 +1,37 @@
-axios.get('https://api.github.com/repos/THZoria/NX_Firmware/releases')
-  .then(response => {
-    const data = response.data;
-    const versions = data.map(release => release.tag_name);
+      // 使用 axios 发送请求获取数据
+      axios.get('https://api.github.com/repos/THZoria/NX_Firmware/releases')
+        .then(response => {
+          const data = response.data;
+          const versions = data.map(release => release.tag_name);
 
-    const versionSelect = document.getElementById('versions');
-    versions.forEach(version => {
-      const option = document.createElement('option');
-      option.value = version;
-      option.textContent = '固件版本 ' + version; // 加上固件版本号文字
-      versionSelect.appendChild(option);
-    });
+          const versionDropdownMenu = document.getElementById('versionsDropdownMenu');
+          versions.forEach(version => {
+            const optionLink = document.createElement('a');
+            optionLink.className = 'dropdown-item';
+            optionLink.href = '#';
+            optionLink.textContent = '固件版本 ' + version;
+            optionLink.onclick = () => {
+              document.getElementById('versionsDropdown').textContent = '固件版本 ' + version;
+              return false;
+            };
+            versionDropdownMenu.appendChild(optionLink);
+          });
 
-    const downloadButton = document.getElementById('downloadButton');
-    downloadButton.addEventListener('click', () => {
-      const version = versionSelect.value;
-      if (version) {
-        const release = data.find(release => release.tag_name === version);
-        const asset = release.assets[0];
-        const proxyUrl = 'https://ghproxy.com/';
-        const link = proxyUrl + asset.browser_download_url;
-        window.open(link);
-      }
-    });
-    const firstVersion = versions[0];
-    versionSelect.value = firstVersion;
-  })
-  .catch(error => {
-    console.log(error);
-  });
+          const downloadButton = document.getElementById('downloadButton');
+          downloadButton.addEventListener('click', () => {
+            const version = document.getElementById('versionsDropdown').textContent.replace(/^固件版本\s/, ''); // 去掉前缀 '固件版本 '
+            if (version) {
+              const release = data.find(release => release.tag_name === version);
+              const asset = release.assets[0];
+              const proxyUrl = 'https://ghproxy.com/';
+              const link = proxyUrl + asset.browser_download_url;
+              window.open(link);
+            }
+          });
+
+          const firstVersion = versions[0];
+          document.getElementById('versionsDropdown').textContent = '固件版本 ' + firstVersion;
+        })
+        .catch(error => {
+          console.log(error);
+        });
