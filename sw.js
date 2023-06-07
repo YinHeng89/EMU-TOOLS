ebd072ab269c4c93fcc82e6461b120c23da2b8de
YinHeng89 <k332159915@gmail.com>
2023-06-07 11:57:35 +0800

    更新版本

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function (e) {
 
     console.log('现在正在请求：' + e.request.url);
-    const cacheName = 'EMU-Tools-v1.0.3.3';
+    const cacheName = 'EMU-Tools-v1.0.3.4';
 });
