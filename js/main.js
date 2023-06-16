7f95b123d9311643faf5dcce00d3a0fbea89f933
YinHeng89 <k332159915@gmail.com>
2023-06-16 14:29:19 +0800

    更新 main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ if ('serviceWorker' in navigator) {
 //获取访问数据（因为跨域和SSL问题，暂不能实现）
 $(document).ready(function () {
     // 发送 AJAX GET 请求获取 PHP 文件返回的数据
-    $.get("https://count.emu-tools.com/tools/count.php", function (data) {
+    $.get("https://count.emu-tools.com/", function (data) {
         // 将返回的数据插入到 HTML 中
         $("#data-container").html(data);
     });
