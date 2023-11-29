6f9cf0b2477e0593fdcdb099aea6d7be992ac627
YinHeng89 <k332159915@gmail.com>
2023-11-29 14:00:16 +0800

    Update yuzu.js

--- js/yuzu.js
+++ js/yuzu.js
@@ -46,8 +46,8 @@ async function downloadLatest(os) {
 
   // 从 API 响应中获取下载链接
   const osEm = {
-    Windows: `https://gh-proxy.com/${latestRelease.assets.find(asset => asset.name.endsWith('.zip') && !asset.name.endsWith('-debugsymbols.zip')).browser_download_url}`,
-    Linux: `https://gh-proxy.com/${latestRelease.assets.find(asset => asset.name.endsWith('.AppImage')).browser_download_url}`,
+    Windows: `latestRelease.assets.find(asset => asset.name.endsWith('.zip') && !asset.name.endsWith('-debugsymbols.zip')).browser_download_url`,
+    Linux: `latestRelease.assets.find(asset => asset.name.endsWith('.AppImage')).browser_download_url`,
     Android: 'https://play.google.com/store/apps/details?id=org.yuzu.yuzu_emu'
   };
 
