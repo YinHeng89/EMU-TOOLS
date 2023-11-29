917bfc31dd5ed18c1a771d31086c9b7b6b2e6c72
YinHeng89 <k332159915@gmail.com>
2023-11-29 13:56:56 +0800

    Update firmware.js

--- js/firmware.js
+++ js/firmware.js
@@ -23,8 +23,7 @@
             if (version) {
               const release = data.find(release => release.tag_name === version);
               const asset = release.assets[0];
-              const proxyUrl = 'https://gh-proxy.com/';
-              const link = proxyUrl + asset.browser_download_url;
+              const link = asset.browser_download_url;
               window.open(link);
             }
           });
